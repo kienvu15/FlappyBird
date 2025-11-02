9a4dc1b976059cd32e53e5da1d941020f8d16757
kienvu15 <88vutrunkien@gmail.com>
2025-11-02 15:13:43 +0700

    up

--- Assets/Code/Menu.cs
+++ Assets/Code/Menu.cs
@@ -1,16 +1,44 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject butonObject;
+    public GameObject pauseButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        gameObject.SetActive(false);
+        butonObject.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void TapIt()
+    {
+        butonObject.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void pasueButton()
+    {
+        SceneManager.LoadScene(0);
     }
 }
